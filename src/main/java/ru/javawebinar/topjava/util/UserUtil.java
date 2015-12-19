91f655066725a83ddf9f0d109cfa7033a52164b5
ArturDS <arturdsoma@gmail.com>
2015-12-20 00:46:55 +0300

    hw try number 2

--- src/main/java/ru/javawebinar/topjava/util/UserUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserUtil.java
@@ -1,7 +1,27 @@
 package ru.javawebinar.topjava.util;
 
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Created by ArturDS on 20.12.2015.
+ * Created by ArturDS on 16.12.2015.
  */
 public class UserUtil {
+
+    public static final List<User> USERS_LIST = Arrays.asList(
+            new User(1,"fsdfadff", "abc@gmail.com", "Сегей", Role.ROLE_USER),
+            new User(2,"fsdfaf", "vgz@gmail.com", "Василий", Role.ROLE_ADMIN),
+            new User(3,"fsdaaf", "rms@gmail.com", "Андрей", Role.ROLE_USER),
+            new User(5,"fsdfff", "bg@gmail.com", "Герман", Role.ROLE_USER),
+            new User(4,"fsdf", "fd@gmail.com", "Артем", Role.ROLE_USER),
+            new User(6,"fsdf", "af@gmail.com", "Вениамин", Role.ROLE_USER)
+    );
+
+    public static void main(String[] args) {
+        System.out.println(USERS_LIST);
+    }
+
 }
