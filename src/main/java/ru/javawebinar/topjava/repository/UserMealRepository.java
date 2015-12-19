e822da60fa5cb4739099aba9c504b5ba7e8855a5
ArturDS <arturdsoma@gmail.com>
2015-12-20 01:21:20 +0300

    hw try number 2

--- src/main/java/ru/javawebinar/topjava/repository/UserMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/UserMealRepository.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.repository;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.UserMeal;
 
 import java.util.Collection;
@@ -8,6 +10,7 @@ import java.util.Collection;
  * GKislin
  * 06.03.2015.
  */
+
 public interface UserMealRepository {
     UserMeal save(UserMeal userMeal);
 
