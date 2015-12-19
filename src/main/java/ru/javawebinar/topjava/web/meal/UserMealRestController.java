ae45bc5ea4ed19c86dcdbf6e571ca7132db9e73a
ArturDS <arturdsoma@gmail.com>
2015-12-20 01:09:02 +0300

    hw try number 2

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -1,14 +1,49 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.LoggerWrapper;
+import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.service.UserMealServiceImpl;
+
+import java.util.List;
 
 /**
  * GKislin
  * 06.03.2015.
  */
+
+//TODO Дописать контроллер
 @Controller
 public class UserMealRestController {
+    protected final LoggerWrapper LOG = LoggerWrapper.get(getClass());
+
+    @Autowired
     private UserMealServiceImpl service;
 
+    public List<UserMeal> getAll() {
+        LOG.info("getAll");
+        return service.getAll();
+    }
+
+    public UserMeal get(int id) {
+        LOG.info("get " + id);
+        return service.get(id);
+    }
+
+    public UserMeal create(UserMeal userMeal) {
+        LOG.info("create " + userMeal);
+        return service.save(userMeal);
+    }
+
+    public void delete(int id) {
+        LOG.info("delete " + id);
+        service.delete(id);
+    }
+
+    public void update(UserMeal userMeal, int id) {
+        userMeal.setId(id);
+        LOG.info("update " + userMeal);
+        service.update(userMeal);
+    }
 }
