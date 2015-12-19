27bb83e62bfe539b687d85d90ea86f9b49c4d014
ArturDS <arturdsoma@gmail.com>
2015-12-20 00:44:40 +0300

    hw try number 2

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -3,12 +3,16 @@ package ru.javawebinar.topjava.repository.mock;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.LoggerWrapper;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.UserMealsUtil;
+import ru.javawebinar.topjava.util.UserUtil;
+import ru.javawebinar.topjava.util.exception.ExceptionUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * GKislin
@@ -17,35 +21,60 @@ import java.util.concurrent.ConcurrentHashMap;
 @Repository
 public class InMemoryUserRepositoryImpl implements UserRepository {
     private static final LoggerWrapper LOG = LoggerWrapper.get(InMemoryUserRepositoryImpl.class);
-    private Map<Integer,User> repository = new ConcurrentHashMap<>();
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+
+    {
+        UserUtil.USERS_LIST.forEach(this::save);
+    }
 
     @Override
     public boolean delete(int id) {
         LOG.info("delete " + id);
+        repository.remove(id);
         return true;
     }
 
     @Override
     public User save(User user) {
         LOG.info("save " + user);
+        repository.put(user.getId(), user);
         return user;
     }
 
     @Override
     public User get(int id) {
         LOG.info("get " + id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         LOG.info("getAll");
-        return Collections.emptyList();
+        List<User> userList = new ArrayList<User>(repository.values());
+        Collections.sort(userList, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        return userList;
     }
 
     @Override
     public User getByEmail(String email) {
         LOG.info("getByEmail " + email);
-        return null;
+        User user = null;
+        for (Map.Entry<Integer, User> entry : repository.entrySet()) {
+            if (entry.getValue().getEmail().equals(email)) {
+                user = entry.getValue();
+            }
+        }
+        if (user == null) {
+            throw new NotFoundException("User with " + email + "not found");
+
+        } else {
+            return user;
+        }
+
     }
 }
