5acb7ec691be678dad39e0c061a62dec8a2a170e
ArturDS <arturdsoma@gmail.com>
2015-12-20 00:46:01 +0300

    hw try number 2

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -1,11 +1,13 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.service.UserMealServiceImpl;
 
 /**
  * GKislin
  * 06.03.2015.
  */
+@Controller
 public class UserMealRestController {
     private UserMealServiceImpl service;
 
